ef7946e731a8c1e1ccf58822402a66573dd0110f
soundofeden <54005303+soundofeden@users.noreply.github.com>
2019-08-13 00:42:47 +0300

    import build

---
+++ precache-manifest.aa49e4ea62863198bde9c44cb8c8b798.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b0e68014e95cd90392630e3ec328c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90af761453077a1952de",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "041f74da1611840119c1",
+    "url": "/static/css/main.01b3fac4.chunk.css"
+  },
+  {
+    "revision": "90af761453077a1952de",
+    "url": "/static/js/2.c355f6ed.chunk.js"
+  },
+  {
+    "revision": "041f74da1611840119c1",
+    "url": "/static/js/main.e075acf5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "42e987c4314fab0fd18b80962d63c05d",
+    "url": "/static/media/Group 5.42e987c4.svg"
+  },
+  {
+    "revision": "ba34306cf415850dadbea43aa7025a3b",
+    "url": "/static/media/about.ba34306c.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
